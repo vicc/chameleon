d48143c677ef23982e855bb33dcfa43aa45bbc1c
bre7 <mobileuy@gmail.com>
2015-09-25 22:42:53 -0300

    Added Demo

---
+++ ChameleonDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ChameleonDemo
+//
+//  Created by Vicc Alexander on 9/25/15.
+//  Copyright © 2015 Vicc Alexander. All rights reserved.
+//
+
+import UIKit
+import Chameleon
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = FlatYellow()
+    }
+
+}
+
