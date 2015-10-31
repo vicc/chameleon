a72014e8314c1852c9ebb48d4fb023eb6e8025fa
Dan Loewenherz <dloewenherz@gmail.com>
2015-10-31 16:32:37 -0500

    use Objective-C 2.0 subscripting for NSArray

--- Pod/Classes/Objective-C/UIColor+Chameleon.m
+++ Pod/Classes/Objective-C/UIColor+Chameleon.m
@@ -701,7 +701,7 @@
             
             NSArray *darkColors = @[FlatBlackDark, FlatBlueDark, FlatBrownDark, FlatCoffeeDark, FlatForestGreenDark, FlatGrayDark, FlatGreenDark, FlatLimeDark, FlatMagentaDark, FlatMaroonDark, FlatMintDark, FlatNavyBlueDark, FlatOrangeDark, FlatPinkDark, FlatPlumDark, FlatPowderBlueDark, FlatPurpleDark, FlatRedDark, FlatSandDark, FlatSkyBlueDark, FlatTealDark, FlatWatermelonDark, FlatWhiteDark, FlatYellowDark];
             
-            randomColor = [darkColors objectAtIndex:randomColorChosen];
+            randomColor = darkColors[randomColorChosen];
             break;
         }
             
@@ -710,7 +710,7 @@
         
             NSArray *lightColors = @[FlatBlack, FlatBlue, FlatBrown, FlatCoffee, FlatForestGreen, FlatGray, FlatGreen, FlatLime, FlatMagenta, FlatMaroon, FlatMint, FlatNavyBlue, FlatOrange, FlatPink, FlatPlum, FlatPowderBlue, FlatPurple, FlatRed, FlatSand, FlatSkyBlue, FlatTeal, FlatWatermelon, FlatWhite, FlatYellow];
             
-            randomColor = [lightColors objectAtIndex:randomColorChosen];
+            randomColor = lightColors[randomColorChosen];
             break;
         }
     }
